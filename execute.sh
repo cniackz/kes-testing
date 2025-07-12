5013809f684ee953f5308fa75e1ad97e877e34c5
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2025-07-11 18:15:39 -0600

    improving usability

--- execute.sh
+++ execute.sh
@@ -93,15 +93,26 @@ kubectl exec $(kubectl get pods -l app=vault  | grep -v NAME | awk '{print $1}')
 
 # Success! Data written to: auth/approle/role/kes-role
 kubectl exec $(kubectl get pods -l app=vault  | grep -v NAME | awk '{print $1}') -- sh -c 'VAULT_TOKEN='$VAULT_ROOT_TOKEN' VAULT_ADDR="http://127.0.0.1:8200" vault write auth/approle/role/kes-role token_num_uses=0 secret_id_num_uses=0 period=5m policies=kes-policy'
+sleep 3  # Después de vault write policy
 
 # Expected: 003ea2c3-983d-e283-1f93-0c6baa2a710e
+sleep 3  # Antes de leer role_id
 ROLE_ID=$(kubectl exec $(kubectl get pods -l app=vault  | grep -v NAME | awk '{print $1}') -- sh -c 'VAULT_TOKEN='$VAULT_ROOT_TOKEN' VAULT_ADDR="http://127.0.0.1:8200" vault read auth/approle/role/kes-role/role-id' | grep "role_id    " | sed -e "s/role_id    //g")
 echo "ROLE_ID: ${ROLE_ID}"
 
 # Expected: 0f4289bd-6fb0-4271-483f-0c7d0d1cce44
 SECRET_ID=$(kubectl exec $(kubectl get pods -l app=vault  | grep -v NAME | awk '{print $1}') -- sh -c 'VAULT_TOKEN='$VAULT_ROOT_TOKEN' VAULT_ADDR="http://127.0.0.1:8200" vault write -f auth/approle/role/kes-role/secret-id' | grep "secret_id             " | sed -e "s/secret_id             //g")
+sleep 3  # Después de vault write policy
 echo "SECRET_ID: ${SECRET_ID}"
 
+
+echo "ROLE_ID: ${ROLE_ID}"
+echo "SECRET_ID: ${SECRET_ID}"
+read -p "¿Se ven bien los valores? Presiona 'y' para continuar: " confirm
+if [ "$confirm" != "y" ]; then
+  echo "Abortando script..."
+  exit 1
+fi
 
 
 
